bc446513f9226bd0ff0b4285872ed42679d2e54c
Bpedley <v3rtex12@gmail.com>
2019-10-22 20:15:13 +0300

    Updates

---
+++ precache-manifest.0ba84d4a13e2a0173d03a034f66975e0.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2e6971fed5a5567898bdcc5d17379e6f",
+    "url": "/jsonplaceholder-api-cards/index.html"
+  },
+  {
+    "revision": "acd1c4ab62494cae811a",
+    "url": "/jsonplaceholder-api-cards/static/css/2.72ee3d1d.chunk.css"
+  },
+  {
+    "revision": "1f55355663a6333e00d6",
+    "url": "/jsonplaceholder-api-cards/static/css/main.eb72e876.chunk.css"
+  },
+  {
+    "revision": "acd1c4ab62494cae811a",
+    "url": "/jsonplaceholder-api-cards/static/js/2.3ab12764.chunk.js"
+  },
+  {
+    "revision": "1f55355663a6333e00d6",
+    "url": "/jsonplaceholder-api-cards/static/js/main.f2f84e08.chunk.js"
+  },
+  {
+    "revision": "08244c6d1d47ad7dbbb4",
+    "url": "/jsonplaceholder-api-cards/static/js/runtime-main.c6f39e99.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/jsonplaceholder-api-cards/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/jsonplaceholder-api-cards/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/jsonplaceholder-api-cards/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/jsonplaceholder-api-cards/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/jsonplaceholder-api-cards/static/media/fontawesome-webfont.fee66e71.woff"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/jsonplaceholder-api-cards/precache-manifest.0ba84d4a13e2a0173d03a034f66975e0.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/jsonplaceholder-api-cards/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
+});
